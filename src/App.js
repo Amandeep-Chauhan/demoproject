bbb68fbfda129ea668ffa393b4e12339fa27760e
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-06-05 23:37:34 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import './App.css';
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Layout from './components/Layout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "/details",
+        children: [
+          {
+            index: true,
+            element: <div>details</div>,
+          },
+          {
+            path: "/details/:id",
+            element: <div>details: id</div>,
+          },
+        ],
+      },
+      {
+        path: "/category",
+        errorElement: <div>error comp</div>,
+        element: <div>details</div>,
+      }
+    ],
+  },
+
+]);
+
 function App() {
   return (
     <div className="App">
-          Learn React
+          <RouterProvider router={router} />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Layout/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+
+const Layout = () => {
+    return (
+        <div>
+             <div>
+            header
+            </div>
+            <div style={{
+                width: '100%',
+                height: '400px',
+                backgroundColor: 'red',
+            }}>
+            <Outlet />
+            </div>
+        </div>
+    )
+} 
+
+export default Layout;
